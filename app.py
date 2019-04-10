cdeec2d4a2b75744ccbbb55343132bd41ce8e18d
tylerbattler <tychriste@gmail.com>
2019-04-10 10:57:32 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import flask_cors
+from flask_heroky import HEROKU
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_uri'] = ''
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "<h1>hello from flask</h1>"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
