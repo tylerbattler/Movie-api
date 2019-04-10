76c9bcd9f19754dcaf5029ac9363b58dc786fc62
tylerbattler <tychriste@gmail.com>
2019-04-10 13:27:33 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_heroky import HEROKU
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
